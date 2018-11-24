4d0f65aa6692b5a029504366bd59685828ee786b
Alex <j.alexander.lehman@gmail.com>
2018-11-24 12:13:32 -0500

    updates

--- p2-2/main.cpp
+++ p2-2/main.cpp
@@ -123,6 +123,41 @@ int get_minor_action(GameInputs inputs) // Decides actions between NPCs
 #define HALFDRAW 3
 #define GODMODE 4
 
+int load_game() {
+    return(0);
+}
+
+void save_game() {
+    int b;
+    for(b = 0; b < 10; b++){
+    
+      
+   }
+}
+
+
+void lostlife()
+{ // Runs when the player loses a life
+    // Actually removes the life from the player
+    Player.lives = Player.lives - 1;
+
+    // Plays lost life sound
+    FILE *wave_file;
+    wave_file = fopen("/sd/roblox_death_sound.wav", "r");
+    waver.play(wave_file);
+    fclose(wave_file);
+    int lives = Player.lives;
+
+    // Show text
+    uLCD.filled_rectangle(Player.x, Player.y, Player.x + 10, Player.y + 10, 0xFF0000);
+    uLCD.filled_rectangle(Player.x + 1, Player.y + 1, Player.x + 9, Player.y + 9, 0x000000);
+    uLCD.locate(Player.x + 2, Player.y + 2);
+    uLCD.printf("You lost a life!");
+    uLCD.locate(Player.x + 6, Player.y + 6);
+    uLCD.printf("Only %d lives remaining", lives);
+    draw_game(true);
+}
+
 /**
  * Entry point for frame drawing. This should be called once per iteration of
  * the game loop. This draws all tiles on the screen, followed by the status 
