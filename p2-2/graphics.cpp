263e4137586193b3aaf7fcf62f4a5c409e90887f
Alex <j.alexander.lehman@gmail.com>
2018-11-25 18:21:33 -0500

    change

--- p2-2/graphics.cpp
+++ p2-2/graphics.cpp
@@ -30,139 +30,6 @@
 //     wait_us(250); // Recovery time!
 // }
 
-void draw_nothing(int u, int v)
-{
-    // Fill a tile with blackness
-    uLCD.filled_rectangle(u, v, u + 10, v + 10, BLACK);
-}
-
-// void draw_pause()
-// {
-//     // Fill a tile with blackness
-//     uLCD.BLIT(0, 0, 128, 128, *pause_screen);
-// }
-
-// void draw_splash()
-// {
-//     // Fill a tile with blackness
-//     uLCD.BLIT(0, 0, 128, 128, *splash);
-// }
-
-void draw_wall(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *wall);
-    //wait_us(250); // Recovery time!
-}
-
-void draw_success(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11, *success);
-    //wait_us(250); // Recovery time!
-}
-
-// void draw_plant(int u, int v)
-// {
-//     img = plant;
-//     uLCD.BLIT(u, v, 11, 11, img);
-// }
-
-void draw_mainChar(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *mainChar);
-}
-
-void draw_door(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *door);
-}
-
-void draw_plant(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *plant);
-}
-
-void draw_door_open(int u, int v)
-{
-    //uLCD.BLIT(u, v, 11, 11, door_open);
-    uLCD.BLIT(u, v, 11, 11,  *door_open);
-}
-
-void draw_UGA_student(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *UGA_student);
-}
-
-void draw_depression(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *depression);
-}
-
-void draw_failure(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *failure);
-}
-
-void draw_anxiety(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *anxiety);
-}
-
-
-void draw_pWills(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *pWills);
-}
-
-void draw_pSchimmel(int u, int v)
-{
-    uLCD.BLIT(u, v, 11, 11,  *pSchimmel);
-}
-
-void draw_end(int status)
-{
-    const char* line1 = "GAME OVER";
-    if (!status)
-    {
-        uLCD.locate(10, 10);
-        uLCD.printf("YOU LOST");
-    }
-    else
-    {
-        uLCD.locate(10, 10);
-        uLCD.printf("YOU WIN");
-    }
-}
-
-void draw_upper_status(int x, int y)
-{
-    // Draw bottom border of status bar
-    uLCD.line(0, 9, 127, 9, GREEN);
-
-    // Add other status info drawing code here
-    uLCD.locate(0, 0);
-    uLCD.printf("X coord = %d, Y coord = %d\n", x, y);
-}
-
-void draw_lower_status(char* line, int lives)
-{
-    // Draw top border of status bar
-    uLCD.line(0, 118, 127, 118, GREEN);
-
-    // Add other status info drawing code here
-    uLCD.locate(0, 122);
-    uLCD.printf(line); // Information on whether the player has the key (sign off) yet
-    uLCD.locate(90, 122);
-    uLCD.printf("Lives: %d", lives); // Information on whether the player has the key (sign off) yet
-}
-
-void draw_border()
-{
-    uLCD.filled_rectangle(0, 9, 127, 14, WHITE);     // Top
-    uLCD.filled_rectangle(0, 13, 2, 114, WHITE);     // Left
-    uLCD.filled_rectangle(0, 114, 127, 117, WHITE);  // Bottom
-    uLCD.filled_rectangle(124, 14, 127, 117, WHITE); // Right
-}
-
 int mainChar[1][121] = {
 {
 0x00000000, 0x00000000, 0x00000000, 0xff182932, 0xff182932, 0xff182932, 0xff182932, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 
@@ -355,3 +222,137 @@ int anxiety[1][121] = {
 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73, 0xff238f73
 }
 };
+
+void draw_nothing(int u, int v)
+{
+    // Fill a tile with blackness
+    uLCD.filled_rectangle(u, v, u + 10, v + 10, BLACK);
+}
+
+// void draw_pause()
+// {
+//     // Fill a tile with blackness
+//     uLCD.BLIT(0, 0, 128, 128, *pause_screen);
+// }
+
+// void draw_splash()
+// {
+//     // Fill a tile with blackness
+//     uLCD.BLIT(0, 0, 128, 128, *splash);
+// }
+
+void draw_wall(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *wall);
+    //wait_us(250); // Recovery time!
+}
+
+void draw_success(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11, *success);
+    //wait_us(250); // Recovery time!
+}
+
+// void draw_plant(int u, int v)
+// {
+//     img = plant;
+//     uLCD.BLIT(u, v, 11, 11, img);
+// }
+
+void draw_mainChar(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *mainChar);
+}
+
+void draw_door(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *door);
+}
+
+void draw_plant(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *plant);
+}
+
+void draw_door_open(int u, int v)
+{
+    //uLCD.BLIT(u, v, 11, 11, door_open);
+    uLCD.BLIT(u, v, 11, 11,  *door_open);
+}
+
+void draw_UGA_student(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *UGA_student);
+}
+
+void draw_depression(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *depression);
+}
+
+void draw_failure(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *failure);
+}
+
+void draw_anxiety(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *anxiety);
+}
+
+
+void draw_pWills(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *pWills);
+}
+
+void draw_pSchimmel(int u, int v)
+{
+    uLCD.BLIT(u, v, 11, 11,  *pSchimmel);
+}
+
+void draw_end(int status)
+{
+    const char* line1 = "GAME OVER";
+    if (!status)
+    {
+        uLCD.locate(10, 10);
+        uLCD.printf("YOU LOST");
+    }
+    else
+    {
+        uLCD.locate(10, 10);
+        uLCD.printf("YOU WIN");
+    }
+}
+
+void draw_upper_status(int x, int y)
+{
+    // Draw bottom border of status bar
+    uLCD.line(0, 9, 127, 9, GREEN);
+
+    // Add other status info drawing code here
+    uLCD.locate(0, 0);
+    uLCD.printf("X coord = %d, Y coord = %d\n", x, y);
+}
+
+void draw_lower_status(char* line, int lives)
+{
+    // Draw top border of status bar
+    uLCD.line(0, 118, 127, 118, GREEN);
+
+    // Add other status info drawing code here
+    uLCD.locate(0, 122);
+    uLCD.printf(line); // Information on whether the player has the key (sign off) yet
+    uLCD.locate(90, 122);
+    uLCD.printf("Lives: %d", lives); // Information on whether the player has the key (sign off) yet
+}
+
+void draw_border()
+{
+    uLCD.filled_rectangle(0, 9, 127, 14, WHITE);     // Top
+    uLCD.filled_rectangle(0, 13, 2, 114, WHITE);     // Left
+    uLCD.filled_rectangle(0, 114, 127, 117, WHITE);  // Bottom
+    uLCD.filled_rectangle(124, 14, 127, 117, WHITE); // Right
+}
+
