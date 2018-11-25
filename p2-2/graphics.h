030ae4e47e73aed41c905beb7805eea505dcce22
Alex <j.alexander.lehman@gmail.com>
2018-11-25 12:08:59 -0500

    Update graphics.h

--- p2-2/graphics.h
+++ p2-2/graphics.h
@@ -6,23 +6,6 @@
  */
 void draw_player(int u, int v);
 
-
-/**
- * Takes a string image and draws it to the screen. The string is 121 characters
- * long, and represents an 11x11 tile in row-major ordering (across, then down,
- * like a regular multi-dimensional array). The available colors are:
- *      R = Red
- *      Y = Yellow
- *      G = Green
- *      D = Brown ("dirt")
- *      5 = Light grey (50%)
- *      3 = Dark grey (30%)
- *      Any other character is black
- * More colors can be easily added by following the pattern already given.
- */
-void conv_img(int u, int v, int image);
-void conv_img_2(int image);
-
 /**
  * DrawFunc functions. 
  * These can be used as the MapItem draw functions.
